a7366d4a8784283c2a9d68e16226fc7387db4aeb
bun <vlongbf@gmail.com>
2024-12-28 12:36:25 +0900

    更新

--- build/mypkg/prefix_override/sitecustomize.py
+++ build/mypkg/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/bun/ros2_7bai_ws/install/mypkg'
+    sys.prefix = sys.exec_prefix = '/home/bun/ros2_ws/install/mypkg'
