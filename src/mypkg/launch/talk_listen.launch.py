44f97321a632bc36134429f1c4ff8d90f5114d42
bun <vlongbf@gmail.com>
2024-12-28 11:52:49 +0900

    3

--- src/mypkg/launch/talk_listen.launch.py
+++ src/mypkg/launch/talk_listen.launch.py
@@ -6,11 +6,11 @@ import launch_ros.actions
 
 def generate_launch_description():
 
-    talker = launch_ros.actions.Node(
+    number = launch_ros.actions.Node(
         package = 'mypkg',
         executable = 'number',
     )
-    listener = launch_ros.actions.Node(
+    answer = launch_ros.actions.Node(
         package = 'mypkg',
         executable = 'answer',
         output = 'screen',
